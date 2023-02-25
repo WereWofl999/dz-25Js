597fcb56c95359ee04f3e743f6c78c2588af38da
Vyacheslav Gavriluk <vyacheslavgavriluk@Air-Vyacheslav.lan>
2023-02-25 12:48:00 +0200

    Js

---
+++ age.js
@@ -0,0 +1,7 @@
+
+
+const currentYear = 2023;//текущий год
+
+let berthYear = prompt('Введіть ваш рік народження.');// Введіть ваш рік народження.
+let age = currentYear - berthYear ;//вычесляет возраст 
+alert(`Ваш рік народження від ${berthYear} ріків від ${age} лет.`);// Финальный результат 
